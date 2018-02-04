b09adcddb9ac142f19f2805299af82df5a9627ec
Jonathan Hardison <jmh@jonathanhardison.com>
2018-02-04 16:41:50 -0600

    rough setup

---
+++ src/custom/VGP-BulkLoad/create-vpgbulkload.ps1
@@ -0,0 +1,52 @@
+Install-Module -Name ZertoModule
+Import-Module ZertoModule
+
+Connect-ZertoZVM -ZertoServer "il1zerto.test.com" -ZertoUser  "Test\\ZertoUser"
+
+#Putting it all together
+$VPGFile = '.\vpg-master.csv'
+$VMFile = '.\vm-master.csv'
+
+$VPGList = Get-Content $VPGFile | ConvertFrom-Csv
+$VMList = Get-Content $VMFile | ConvertFrom-Csv
+
+$VPGList | ForEach-Object {
+    #VPG Variables
+    $VPGName = $_.VPGName
+    $RecoverySiteName = $_.RecoverySiteName
+    $ClusterName = $_.ClusterName
+    $FailoverNetwork = $_.FailoverNetwork
+    $TestNetwork = $_.TestNetwork
+    $DatastoreName = $_.DatastoreName
+    $Folder = $_.Folder
+
+    #create an array
+    $VMs = @()
+    $VMList | Where-Object {$_.VPGName -eq $VPGName} | ForEach-Object {
+        #Make Recovery Object        
+        $VPGRecovery = New-VPGVMRecovery -FolderIdentifier $_.Folder
+
+        #Make IP Object
+        $IP = New-ZertoVPGFailoverIPAddress -NICName $_.NICName `
+                    -TestNetworkID $_.TestNetwork `
+                    -NetworkID $_.FailoverNetwork
+
+        #Make VM Object
+        $VM = New-ZertoVPGVirtualMachine -VMName $_.VMName -VPGFailoverIPAddress $IP -VPGVMRecovery $VPGRecovery
+
+        #Add VM to array
+        $VMs += $VM
+    }
+
+    #Create VPG with VM array
+    Add-ZertoVPG -Priority Low `
+        -VPGName $_.VPGName `
+        -RecoverySiteName $_.RecoverySiteName `
+        -ClusterName $_.ClusterName `
+        -FailoverNetwork $_.FailoverNetwork `
+        -TestNetwork $_.TestNetwork `
+        -DatastoreName $_.DatastoreName `
+        -Folder $_.Folder `
+        -JournalUseDefault:$true `
+        -VPGVirtualMachines $VMs
+}
